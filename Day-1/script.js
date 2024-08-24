c0796513184a8367b911d6c0f456023220a39f0b
[Naveen Kumar] <nk9411123@gmail.com>
2024-08-24 13:51:07 +0530

    Uploading my  files on Git-Hub "

---
+++ Day-1/script.js
@@ -0,0 +1,109 @@
+// gsap.to("#box1",{
+//     x:1200,
+//     duration:2,
+//     delay:1
+// })
+// gsap.to("#box2",{
+//     x:500,
+//     y:500,
+//     duration:2,
+//     delay:1
+// })
+// gsap.from("#box1",{
+//     x:600,
+//     duration:2,
+//     delay:1,
+// })
+// gsap.from("#box2",{
+//     x:300,
+//     y:300,
+//     duration:2,
+//     delay:1
+// })
+// gsap.to("#box1",{
+//     x:1200,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     backgroundColor:"Blue",
+//     borderRadius:"50%",
+//     scale:0.5
+// })
+// gsap.from("h1",{
+//     // color:"red"
+//     opacity:0,
+//     duration:2,
+//     y:30,
+//     delay:1,
+//     stagger:0.3
+// })
+// gsap.to("#box1",{
+//     x:1000,
+//     duration:2,
+//     delay:1,
+//     rotate:360,
+//     // repeat:2,
+//     repeat:-1,
+//     yoyo:true,
+
+// })
+// gsap.to("#box1",{
+//     x:1500,
+//     rotate:360,
+//     duration:1.5,
+//     delay:1,
+// })
+// gsap.to("#box2",{
+//     x:1500,
+//     backgroundColor:"yellow",
+//     duration:1.5,
+//     delay:2.5,
+// })
+// gsap.to("#box3",{
+//     x:1500,
+//     scale:0.5,
+//     borderRadius:"50%",
+//     duration:1.5,
+//     delay:4
+
+// })
+// var tl = gsap.timeline()
+
+// tl.to("#box1",{
+//     x:1500,
+//     rotate:360,
+//     duration:2,
+//     delay:1,
+// })
+// tl.to("#box2",{
+//     x:1500,
+//     duration:1.5,
+
+// })
+// tl.to("#box3",{
+//     x:1500,
+//     duration:1.5,
+
+// })
+// var tl = gsap.timeline()
+
+// tl.from("h2",{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     delay:0.5
+
+// })
+// tl.from("h4",{
+//     y:-20,
+//     opacity:0,
+//     duration:1,
+//     stagger:0.3
+
+// })
+// tl.from("h1",{
+//     y:20,
+//     opacity:0,
+//     duration:0.5,
+//     scale:0.2
+// })
